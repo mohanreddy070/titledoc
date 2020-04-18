f03d9131b848700113c8c55551e3303574cabf12
mohanreddy070 <mohanreddy070@outlook.com>
2020-04-18 08:31:01 +0530

    2 files modified

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,40 +1,40 @@
-import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators, AbstractControl, ValidatorFn, FormArray } from '@angular/forms';
-
+import { Component, OnInit } from "@angular/core";
+import {
+  FormGroup,
+  FormBuilder,
+  Validators,
+  AbstractControl,
+  ValidatorFn,
+  FormArray
+} from "@angular/forms";
 
 @Component({
-  selector: 'app-form',
-  templateUrl: './form.component.html',
-  styleUrls: ['./form.component.css']
+  selector: "app-form",
+  templateUrl: "./form.component.html",
+  styleUrls: ["./form.component.css"]
 })
 export class FormComponent implements OnInit {
   titleForm: FormGroup;
-
-  constructor(private fb: FormBuilder) { }
+  
+ 
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit() {
     this.titleForm = this.fb.group({
-      orderNumber: ['', [Validators.required, ]],
-      searchDate: ['', [Validators.required, ]],
-      effectiveDate: ['', [Validators.required, ]],
-      addresses: this.buildAddress()
-      
+      orderNumber: ["", [Validators.required]],
+      searchDate: ["", [Validators.required]],
+      effectiveDate: ["", [Validators.required]],
+      address: this.fb.group({
+        street1: "",
+        street2: "",
+        city: "",
+        state: "",
+        zip: "",
+        parcelNo: "",
+        lot: "",
+        block: "",
+        subDivision: ""
+      })
     });
   }
-
-   buildAddress(): FormGroup {
-    return this.fb.group({
-      street1: ['', Validators.required],
-      street2: '',
-      city: '',
-      state: '',
-      zip: '',
-      parcelNo: '',
-      lot: '',
-      block: '',
-      subDivision: ''
-
-    });
-  }
-
 }
