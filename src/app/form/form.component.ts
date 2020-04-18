7ff7746d12544fd5e5ab5a9c84b68229832988b3
mohanreddy070 <mohanreddy070@outlook.com>
2020-04-19 04:17:34 +0530

    2 files modified

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -21,20 +21,50 @@ export class FormComponent implements OnInit {
 
   ngOnInit() {
     this.titleForm = this.fb.group({
-      orderNumber: ["", [Validators.required]],
-      searchDate: ["", [Validators.required]],
-      effectiveDate: ["", [Validators.required]],
-      address: this.fb.group({
-        street1: "",
-        street2: "",
-        city: "",
-        state: "",
-        zip: "",
-        parcelNo: "",
-        lot: "",
-        block: "",
-        subDivision: ""
-      })
+      primaryInfo:this.fb.group({
+        orderNumber: ["", [Validators.required]],
+        searchDate: ["", [Validators.required]],
+        effectiveDate: ["", [Validators.required]],
+        address: this.fb.group({
+          street1: "",
+          street2: "",
+          city: "",
+          state: "",
+          zip: "",
+          parcelNo: "",
+          lot: "",
+          block: "",
+          subDivision: ""
+        })
+      }),
+      deed:this.fb.group({
+
+      }),
+      mortgage:this.fb.group({
+
+      }),
+      judgement:this.fb.group({
+
+      }),
+      tax:this.fb.group({
+        assesmentYear: ["", [Validators.required]],
+        taxYear: ["", [Validators.required]],
+        landValue: ["", [Validators.required]],
+        buildingValue: ["", [Validators.required]],
+        totalValue: ["", [Validators.required]],
+        exemption: ["", [Validators.required]],
+        taxes:this.fb.group({
+          installment: ["", [Validators.required]],
+          amount: ["", [Validators.required]],
+          status: ["", [Validators.required]],
+          paidDueDate: ["", [Validators.required]]
+        }),
+
+      }),
+      additionalInfo:this.fb.group({
+        multipleNames
+      }),
+
     });
   }
 }
